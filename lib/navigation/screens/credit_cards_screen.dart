3e96b56b7f12b983d339d6b794bdc608c2cc9fb9
Madhusudhan <madhusudhan1863@gmail.com>
2024-11-01 19:04:33 +0530

    feat: add search bar in credit cards screen

--- lib/navigation/screens/credit_cards_screen.dart
+++ lib/navigation/screens/credit_cards_screen.dart
@@ -1,7 +1,6 @@
-import 'package:bankdash/modules/cards/barrel.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/home_app_bar.dart';
+import 'package:bankdash/modules/cards/barrel.dart';
 
 class CreditCardsScreen extends StatelessWidget {
   const CreditCardsScreen({super.key});
@@ -9,15 +8,42 @@ class CreditCardsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: HomeAppBar(
-        title: "Credit Cards",
-        onMenuTap: () {
-          Scaffold.of(context).openDrawer();
-        },
-      ),
       body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [];
+        headerSliverBuilder: (ctx, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              title: const Text("Credit Cards",
+                  style: TextStyle(color: Colors.black)),
+              pinned: true,
+              floating: true,
+              backgroundColor: Colors.white,
+              forceElevated: false,
+              scrolledUnderElevation: 0,
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(100),
+                child: Container(
+                  color: Colors.white,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: const SearchBar(
+                    elevation: WidgetStatePropertyAll(0),
+                    backgroundColor: WidgetStatePropertyAll(Color(0xFFF5F7FA)),
+                    leading: Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Icon(Icons.search),
+                    ),
+                    hintText: "Search for something",
+                  ),
+                ),
+              ),
+            ),
+          ];
         },
         body: const CustomScrollView(
           slivers: [
